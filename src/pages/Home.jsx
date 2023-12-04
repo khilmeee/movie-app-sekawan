7ba841d0f3c551f266ee5c9cacf7d1cec4f32d69
KiwilDev <boskhilmiarked123@gmail.com>
2023-12-04 15:22:12 +0700

    nampilin inpo nama kelas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/movie/Header';
 import Movies from './Movies';
 
 const Home = () => {
+    const [showInfo, setShowInfo] = useState(true);
+
+    useEffect(()=> {
+        const udh = setTimeout(()=> {
+            setShowInfo(false);
+        }, 5000);
+        return () => clearTimeout(udh);
+    }, [])
     return (
         <>
+            {
+                showInfo && (
+                    <div>
+                        <p>Hai👋, disini bersama Hilmi</p>
+                        <p>dari XI RPL 1</p>
+                    </div>
+                )
+            }
             <Header />
             <Movies />
         </>
